23efe1903bebfe35f5defc7f484224d9d7fc9847
riprig37 <60636788+riprig37@users.noreply.github.com>
2023-03-15 18:14:47 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById('Lottie'), 
+  renderer: 'svg', // 描画形式
+  loop: true, // trueにしたらループ。1回再生の場合はfalse
+  autoplay: true, // 自動再生
+  path: 'data.json' // jsonのパスを指定
+  name: "Hello World", // 今後のリファレンスとして名前を指定（オプション）
+  });
